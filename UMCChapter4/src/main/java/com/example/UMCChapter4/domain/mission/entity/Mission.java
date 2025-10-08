8dc2526093c3695a2701d9488f4dac9941295e3b
jaeho385 <42105430+jaeho385@users.noreply.github.com>
2025-10-08 23:05:10 +0900

    Add files via upload

---
+++ UMCChapter4/src/main/java/com/example/UMCChapter4/domain/mission/entity/Mission.java
@@ -0,0 +1,41 @@
+package com.example.UMCChapter4.domain.mission.entity;
+
+import com.example.UMCChapter4.domain.member.entity.mapping.MemberMission;
+import com.example.UMCChapter4.domain.store.entity.Store;
+import com.example.UMCChapter4.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table
+public class Mission extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "deadline", nullable = false)
+    private LocalDate deadline;
+
+    @Column(name = "conditions", length = 100, nullable = false)
+    private String condition;
+
+    @Column(name = "point", nullable = false)
+    private int point; // 0 초기화
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.REMOVE) // mission - member_mission (1:N)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY) // mission - store (N:1)
+    @JoinColumn(name = "store_id", foreignKey = @ForeignKey(name = "fk_mission_store_id"))
+    private Store store;
+
+}
