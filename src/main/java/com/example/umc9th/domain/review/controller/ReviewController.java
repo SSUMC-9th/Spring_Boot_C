575878f9c16867a8684cb7e60e96494513c14f36
osy <dhtmddus05@gmail.com>
2025-11-13 13:00:47 +0900

    mission/#07 - API 응답 통일

--- src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc9th/domain/review/controller/ReviewController.java
@@ -2,13 +2,16 @@ package com.example.umc9th.domain.review.controller;
 
 import com.example.umc9th.domain.review.dto.MyReviewDto;
 import com.example.umc9th.domain.review.service.ReviewQueryService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.umc9th.domain.review.dto.MyReviewResDto;
 
-import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +21,15 @@ public class ReviewController {
     private final ReviewQueryService reviewQueryService;
 
     @GetMapping("/my")
-    public List<MyReviewDto> getMyReviews(
+
+    // 반환 타입을 ApiResponse<MyReviewResDto>로 변경
+    public ApiResponse<MyReviewResDto> getMyReviews(
             @RequestParam Long memberId,
             @RequestParam(required = false) String storeName,
             @RequestParam(required = false) Float score
     ) {
+        MyReviewResDto responseDto = reviewQueryService.checkMyReview(memberId, storeName, score);
 
-        return reviewQueryService.checkMyReview(memberId, storeName, score);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, responseDto);
     }
 }
