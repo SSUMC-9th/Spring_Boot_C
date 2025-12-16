d2913ba038505028890f4568379271b3c47960bc
jaeho385 <42105430+jaeho385@users.noreply.github.com>
2025-12-16 19:59:05 +0900

    mission/#10
    AuthenticationManager 추가

--- src/main/java/com/example/UMCChapter4/global/config/SecurityConfig.java
+++ src/main/java/com/example/UMCChapter4/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -95,6 +96,11 @@ public class SecurityConfig {
     public CookieClearingLogoutHandler cookieClearingLogoutHandler() {
         return new CookieClearingLogoutHandler("JSESSIONID");
 //        return new CookieClearingLogoutHandler("Authorization");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 
